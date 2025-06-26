39f72075ac72ca7c401d99856af0354431e6301f
ODIFA JOY <166907019+Timmee016@users.noreply.github.com>
2025-06-26 03:17:13 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-console.log("main.jsx placeholder");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "../style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
