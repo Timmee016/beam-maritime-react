ba9b8db69b8d320d38977ff6d740a517f55efcb7
ODIFA JOY <166907019+Timmee016@users.noreply.github.com>
2025-06-26 02:52:44 +0100

    Delete push-to-github.sh

--- push-to-github.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git init
-git remote add origin https://github.com/Timmee016/beam-maritime-react.git
-git add .
-git commit -m "Initial commit for Vercel deployment"
-git push -u origin master
